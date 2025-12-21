c11948aa8e8ee286fd0b8ade00e3518ceddf457a
DRamGoz <97787556+DRamGoz@users.noreply.github.com>
2025-12-21 19:29:29 +0100

    Update echo.js

--- echo.js
+++ echo.js
@@ -67,7 +67,7 @@ class EventoVisual {
     // verificar que no sea NaN
     if (!isNaN(x) && !isNaN(y) && !isNaN(this.r)) {
       noStroke();
-      fill(0, 180);
+      fill(this.color);
       ellipse(x, y, this.r);
     }
 
@@ -76,6 +76,7 @@ class EventoVisual {
     this.noiseOffsetY += 0.005;
   }
 }
+
 
 
 
