84a68f52c8de76faa743ed4d8ab133c990fe35ea
DRamGoz <97787556+DRamGoz@users.noreply.github.com>
2025-12-25 13:48:54 +0100

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,8 +1,8 @@
 // ==========================
 // CONFIGURACIÓN
 // ==========================
-const NUM_VERTICES_MIN = 20;
-const NUM_VERTICES_MAX = 300;
+const NUM_VERTICES_MIN = 120;
+const NUM_VERTICES_MAX = 500;
 const RADIO_MIN = 10;
 const RADIO_MAX = 120;
 const ALPHA_COLOR = 70;
@@ -299,6 +299,7 @@ class GotaPintura {
     this.noiseY += 0.005;
   }
 }
+
 
 
 
