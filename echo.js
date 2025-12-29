7182b87f50dbb88e3a7e6a1490a4a967586106a6
DRamGoz <97787556+DRamGoz@users.noreply.github.com>
2025-12-29 05:05:17 +0100

    Update echo.js

--- echo.js
+++ echo.js
@@ -302,7 +302,7 @@ class GotaPinturaModo1 {
     this.vertices = [];
     this.noiseX = random(1000);
 this.noiseY = random(1000);
-this.movimiento = 30; // intensidad del desplazamiento
+this.movimiento = 50; // intensidad del desplazamiento
 
     this.color = color(random(255), random(255), random(255), ALPHA_COLOR);
   }
@@ -342,6 +342,7 @@ this.noiseY += 0.004;
 
   }
 }
+
 
 
 
