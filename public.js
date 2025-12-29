c0a1e17a78ec1886bec2ac410bef4111aea0c1d5
DRamGoz <97787556+DRamGoz@users.noreply.github.com>
2025-12-29 20:40:52 +0100

    Update public.js

--- public.js
+++ public.js
@@ -3,14 +3,11 @@ const API_URL = "https://script.google.com/macros/s/AKfycbyTMNP6s4KOhgA_qN4bXCpn
 const btn = document.getElementById("sendBtn");
 
 btn.addEventListener("click", (event) => {
-  // Captura las coordenadas del click
-  const x = event.clientX;
-  const y = event.clientY;
+  const x = event.clientX; // coordenada X del click
+  const y = event.clientY; // coordenada Y del click
+  enviarDato("click", x, y);
+});
 
-  const data = new URLSearchParams();
-  data.append("valor", "click");
-  data.append("x", 123);
-  data.append("y", 456);
 
  function enviarDato(valor, x = 0, y = 0) {
   const data = new URLSearchParams();
@@ -29,6 +26,7 @@ btn.addEventListener("click", (event) => {
   .then(console.log)
   .catch(console.error);
 }
+
 
 
 
