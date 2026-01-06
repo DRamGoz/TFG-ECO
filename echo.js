b9dfb33e122b9774a5ab5a525287060777a12f2f
DRamGoz <97787556+DRamGoz@users.noreply.github.com>
2026-01-07 00:01:17 +0100

    Update echo.js

--- echo.js
+++ echo.js
@@ -116,7 +116,7 @@ if (mostrarOverlay && overlayImg) {
     text(titulo, marcoX + marcoW / 2, marcoY + 20);
     fill(estado.fondoA4 === "blanco" ? 50 : 200);
     textSize(16);
-    text(subtitulo, marcoX + marcoW / 2, marcoY + 30);
+    text(subtitulo, marcoX + marcoW / 2, marcoY + 40); ///// INTERLINEADO SUBTITULO
   }
 
   //==========================
@@ -352,6 +352,7 @@ class GotaPinturaModo1 {
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
 }
+
 
 
 
