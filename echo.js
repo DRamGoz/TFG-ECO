88ca3b4e258b65243190bc1cbe22b73c34f09433
DRamGoz <97787556+DRamGoz@users.noreply.github.com>
2026-02-23 22:50:40 +0100

    Update echo.js

--- echo.js
+++ echo.js
@@ -17,7 +17,7 @@ let titulo = "ECO — Generación de Arte Digital";
 let subtitulo = "Interacción de usuarios en tiempo real";
 let imgFondo;
 // let overlayImg;
-// let mostrarOverlay = false;
+ let mostrarOverlay = false;
 
 
 window.estado = {
@@ -370,6 +370,7 @@ class GotaPinturaModo1 {
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
 }
+
 
 
 
